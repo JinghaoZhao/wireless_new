e616226a38c20c3e920843c376630d8de6ff6bd6
Vincent <zhaojinghao1996@gmail.com>
2017-07-16 10:14:18 +0800

    fix some bugs to suit small screen

--- js/myself.js
+++ js/myself.js
@@ -3,9 +3,11 @@
  */
 window.onscroll = function(){
     screenWidth = $(window).width();
+    // alert(screenWidth);
     screenHeight = $(window).height();
     var t = document.documentElement.scrollTop || document.body.scrollTop;
-    if(screenWidth > 10) {
+    if(screenWidth > 800) {
+        // alert("00000000");
         if( t >= 210 ) {
             $("#nav").addClass("navbar-fixed");
         } else {
